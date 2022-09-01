2e9cfd24cb0c107183217d44b419c1e68938734e
sonyamoonglade <aalexandrovich.hireme@gmail.com>
2022-09-01 20:51:00 +0400

    change key and ip

--- scp.sh
+++ scp.sh
@@ -1,3 +1,5 @@
+cat ./ssh_key && \
+echo $VM_USER && \
 scp -i ./ssh_key ./docker-compose.yaml $VM_USER@$VM_HOST:. && \
 scp -i ./ssh_key ./migrations $VM_USER@$VM_HOST:./migrations && \
 scp -i ./ssh_key ./setup.sh $VM_USER@$VM_HOST:./setup.sh
