4f4c9b4fd517f0dd01d1a42e5ab691007bee1395
sonyamoonglade <aalexandrovich.hireme@gmail.com>
2022-09-02 20:11:01 +0400

    change sh script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-docker-compose -f ./docker-compose.yaml down && \
-docker-compose -f ./docker-compose.yaml pull sonyamoonglade/ci_cd_app:latest && \
-docker-compose -f ./docker-compose.yaml up -d
+docker-compose -f ./compose/docker-compose.yaml down && \
+docker-compose -f ./compose/docker-compose.yaml pull sonyamoonglade/ci_cd_app:latest && \
+docker-compose -f ./compose/docker-compose.yaml up -d
