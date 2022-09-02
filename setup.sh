deef01ba413dff2516b6050347276a3a40f6d779
sonyamoonglade <aalexandrovich.hireme@gmail.com>
2022-09-02 20:47:15 +0400

    change repo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 sudo docker-compose -f ./deployment/docker-compose.yaml down && \
-sudo docker-compose -f ./deployment/docker-compose.yaml pull sonyamoonglade/ci_cd_app:latest && \
+sudo docker-compose -f ./deployment/docker-compose.yaml pull sonyamoonglade/ci_cd_app && \
 sudo docker-compose -f ./deployment/docker-compose.yaml up -d
