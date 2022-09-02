edb2d772485e0532633a695c5430c668edc179ca
sonyamoonglade <aalexandrovich.hireme@gmail.com>
2022-09-02 20:54:21 +0400

    add check

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+
+str=$DB_PWD
+
+for((i=0; i<${#str}; i++)); do
+  ch=${str:$i:1}
+  echo $ch
+done
+
 sudo docker-compose -f ./deployment/docker-compose.yaml down && \
 sudo docker-compose -f ./deployment/docker-compose.yaml pull sonyamoonglade/ci_cd_app && \
 sudo docker-compose -f ./deployment/docker-compose.yaml up -d
