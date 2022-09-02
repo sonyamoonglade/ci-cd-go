c42568366366fcd3a88cf5b6619d1cfcbdbef5d0
sonyamoonglade <aalexandrovich.hireme@gmail.com>
2022-09-02 19:54:10 +0400

    fix ping

--- ping.sh
+++ ping.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-ping -n 1 $VM_HOST > ./result
+timeout 5 ping -n 1 $VM_HOST > ./result
 
 LOST=`cat ./result | grep -ow 'Lost = [0-9]' | grep -ow '[0-9]'`
 
 if test -z $LOST;
   then
-    LOST=0
+    LOST=1
 fi
 
 rm ./result
