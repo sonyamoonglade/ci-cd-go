b0fb3f2cc77e58e92497b5ba800181c1ebcb591d
sonyamoonglade <aalexandrovich.hireme@gmail.com>
2022-09-03 10:09:19 +0400

    add

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mv ./migrations ./deployment/migrations && \
-sudo docker-compose -f ./deployment/docker-compose.yaml down && \
-sudo docker-compose -f ./deployment/docker-compose.yaml pull sonyamoonglade/ci_cd_app && \
+sudo docker-compose -f ./deployment/docker-compose.yaml stop && \
+sudo docker pull sonyamoonglade/ci_cd_app && \
 sudo docker-compose -f ./deployment/docker-compose.yaml --env-file ./.env up -d
